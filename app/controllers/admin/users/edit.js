83a00fa5239877c426f0ea7686effdf583a42011
Matias <matias.a18@gmail.com>
2013-12-15 13:52:20 -0200

    Removed comments

--- app/controllers/admin/users/edit.js
+++ app/controllers/admin/users/edit.js
@@ -28,23 +28,5 @@ module.exports = function(app) {
         }
       });
     });
-    /*var user = new User();
-    user.username = req.body.username;
-    user.brief = req.body.brief;;
-    user.email = req.body.email;
-    User.register(user, req.body.password, function (err) {
-        console.log(err);
-        if (err != null) {
-          // Email is already in use?
-          err.message = (err.message === 'Validation failed')? 'A user with the email ' + user.email + ' is already registered, please take another one' : err.message;
-          return res.render('admin/users/form', {user: user, error_messages: [err.message]});
-        } else {
-          req.flash('info', 'Your user has been created!');
-          res.redirect('/admin/users');
-          res.end();
-          return;
-        }
-      }
-    );*/
   });
 }
