15378163952c71897bafb7c94ab89d5015c54c38
Matias <matias.a18@gmail.com>
2013-12-14 22:54:57 -0200

    Fix for unix servers

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
 
 var user_schema = new mongoose.Schema({
   email: {type: String, unique: true},
-  username: String,
+  username: {type: String, unique: true},
   brief: String
 });
 
