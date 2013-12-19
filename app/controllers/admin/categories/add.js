8304bc979cfd6adb0ddfd4bf2453d449f1334d0b
Matias <matias.a18@gmail.com>
2013-12-19 16:55:31 -0200

    List categories

--- app/controllers/admin/categories/add.js
+++ app/controllers/admin/categories/add.js
@@ -7,8 +7,7 @@ module.exports = function(app) {
   app.get('/admin/categories/add', function(req, res) {
     Category.find({parent: null}).populate('categories').exec(function(err, categories) {
       if (err) throw err;
-      console.log(categories);
-      res.render('admin/categories/form', {title: 'Add Category', category: new Category(), path: '/admin/categories/add', categories: categories, edit: false});  
+      res.render('admin/categories/form', {title: 'Add Category', category: new Category(), path: '/admin/categories/add', categories: categories, edit: false, inputs: true});  
     });
     
   });
@@ -17,14 +16,15 @@ module.exports = function(app) {
     if (req.body.parent_id != 0 && typeof req.body.parent_id != 'undefined') {
       Category.findOne({_id: req.body.parent_id}, function(err, parent_category) {
         if (err) throw err;
-        console.log(parent_category);
         var category = new Category({name: req.body.name, parent: parent_category});
         category.save(function(err, category) {
           if (err) throw err;
-          console.log('saved');
           parent_category.categories.push(category);
           parent_category.save(function(err) {
-            console.log(err);
+            if (err) throw err;
+            res.redirect('/admin/categories');
+            res.end();
+            return;
           });
         });
         
